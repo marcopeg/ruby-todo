3f0d29911fe662e27a1da3e1134dfb7c44b8c94b
Marco Pegoraro <marco.pegoraro@gmail.com>
2015-11-23 13:13:06 +0100

    hash a password field for the user

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,13 @@
+
+require 'bcrypt'
+
 class Author < ActiveRecord::Base
   has_many :task
+
+  # Redefine setter for a specific field
+  def passw=(val)
+    return if val.blank?
+    self['passw'] = BCrypt::Password.create(val)
+  end
+
 end
